62c185d0eb93af5f9c76da61962acca9163e2eed
Zhaohong Liu <jaimefriedhelmzhao@gmail.com>
2024-09-12 11:09:29 +0800

    提交209长度最小的子数组

--- array/MinimumSizeSubarraySum.cc
+++ array/MinimumSizeSubarraySum.cc
@@ -11,13 +11,35 @@ class MinimumSizeSubarraySum final : public BaseSolution {
 public:
     MinimumSizeSubarraySum() : BaseSolution(209, "Minimum Size Subarray Sum") {}
 
-    static int minSubArrayLen(int target, std::vector<int>& nums) {
+    static int minSubArrayLen(const int target, const std::vector<int>& nums) {
+        int sum = 0;
+        int result = INT_MAX;
+        int slowIndex = 0;
+        for (int fastIndex = 0; fastIndex < nums.size(); ++fastIndex) {
+            sum += nums[fastIndex];
+            while (sum >= target) {
+                int subArrayLen = fastIndex - slowIndex + 1;
+                result = std::min(result, subArrayLen);
 
-        return 0;
+                // update slowIndex and sum
+                sum -= nums[slowIndex++];
+            }
+        }
+        return result == INT_MAX ? 0 : result;
     }
 
     void runSolution() override {
         displayProblemInfo();
+        constexpr int target = 7;
+        const std::vector<int> nums = {2, 3, 1, 2, 4, 3};
+
+        std::cout << "The target sum is: " << target << std::endl;
+        std::cout << "The input array is: ";
+        for (const auto& num : nums) {
+            std::cout << num << " ";
+        }
+
+        std::cout << std::endl << "The minimum size of subarray sum is: " << minSubArrayLen(target, nums) << std::endl;
 
     }
 };
